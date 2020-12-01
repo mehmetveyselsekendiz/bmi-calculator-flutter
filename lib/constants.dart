5b63193d22099b30051680d001f7e88b564acd6a
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2020-12-01 22:38:12 +0300

    constants class is created

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 50.0;
+const kBottomContainerColor = Color(0xFFEB1555);
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor= Color(0xFF111328);
+
+const kLabelTextStyle= TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
+const kHeightTextStyle= TextStyle(fontSize: 50.0, fontWeight:FontWeight.w900);
+
+const kSliderThumbColor= Color(0xFFEB1555);
+const kSliderOverlayColor= Color(0x29EB1555); //%16 opacity
+const kSliderActiveColor= Colors.white;
+const kSliderInactiveColor=Color(0xFF8D8E98);
+const kSliderMin= 120;
+const kSliderMax= 220;
